186db7ae2a160f2a983bfb1d9a25673176d6635a
ikraP <ikrapervaiz@gmail.com>
2021-01-09 18:59:16 +0000

    remove dayscene svg

--- src/pages/takeabreak.js
+++ src/pages/takeabreak.js
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { Layout } from "../components/layouts/layout";
 import Break from "../components/sections/Break";
-import DayScene from "../components/sections/BreakScene";
 import BreakScene from "../components/sections/BreakScene";
 
 export default function TakeABreak() {
