36893ad9f85c6eedd4a8229688c2d942b8f5d107
ikraP <ikrapervaiz@gmail.com>
2021-01-09 19:20:35 +0000

    add file url to gatsby-config file for filesystems

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,19 @@ module.exports = {
   siteMetadata: {
     title: "consoleclear",
     siteUrl: "https://www.consoleclear.com",
+
+    description: `A mental health application that aims to reduce anxiety through meditation, breaks and links for help`,
+    author: `IkraP`,
+    siteUrl,
+    lang: `en`,
+    image: "https://www.chemtocode.co.uk/uploads/socialCard.png",
+    favicon: {
+      ico: "/uploads/favicon.ico",
+      sm: "/uploads/favicon-16x16.png",
+      lg: "/uploads/favicon-32x32.png",
+    },
+    twitterUsername: `@chemtocode`,
+    keywords: ["React", "Gatsby", "ChemToCode", "blog", "portfolio"],
   },
   plugins: [
     "gatsby-plugin-styled-components",
@@ -21,6 +34,10 @@ module.exports = {
     },
     {
       resolve: "gatsby-source-filesystem",
+      options: {
+        name: `images`,
+        path: `${__dirname}/static/images`,
+      },
     },
   ],
 };
