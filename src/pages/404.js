965d7c83242486602f0a5fa1e9c7173267e348a6
ikraP <ikrapervaiz@gmail.com>
2021-01-09 18:57:57 +0000

    404 page styling and content addition

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,54 +1,65 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from "react";
+import { Link } from "gatsby";
+import styled from "styled-components";
+import Nightscene from "../components/sections/NightScene";
+import { Layout } from "../components/layouts/layout";
+import "@fontsource/arima-madurai";
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
-const NotFoundPage = () => {
+export default function NotFoundPage() {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
+    <>
+      <Wrapper>
+        <Layout>
+          <TextWrapper>
+            <Text>404 - Page not found</Text>
+            <Link to="/">
+              <LinkText>Go back to home</LinkText>
+            </Link>
+          </TextWrapper>
+          <Nightscene />
+        </Layout>
+      </Wrapper>
+    </>
+  );
 }
 
-export default NotFoundPage
+const Wrapper = styled.div`
+  background: linear-gradient(
+    174.94deg,
+    #9975b3 8.75%,
+    #c25d8c 48.43%,
+    #ecb684 125.46%
+  );
+  width: 100%;
+
+  a {
+    text-decoration: none;
+  }
+`;
+const TextWrapper = styled.div`
+  display: grid;
+  justify-content center;
+
+`;
+
+const Text = styled.p`
+  color: white;
+  font-size: 1.2em;
+  font-family: Arima Madurai;
+  letter-spacing: 0.2em;
+`;
+
+const LinkText = styled.p`
+  text-decoration: none;
+  text-align: center;
+  font-size: 1em;
+  color: #32345e;
+  font-family: Arima Madurai;
+  padding-top: 20px;
+  transition: 0.5s ease-in-out;
+
+  &:hover {
+    color: #aa289d;
+    transform: scale(1.1);
+  }
+`;
