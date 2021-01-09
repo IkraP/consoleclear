9a04bb889f2be494231d27e05a2150c984b383a6
ikraP <ikrapervaiz@gmail.com>
2021-01-09 00:11:55 +0000

    add footer and icons

--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -1,24 +1,68 @@
 import * as React from "react";
 import styled from "styled-components";
+import { RiTwitterFill, RiInstagramFill } from "react-icons/ri";
 
 export default function Footer() {
   return (
     <>
-      <Footer>
-        <a
-          href="https://twitter.com/console.clear"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <i
-            className="ri-twitter-fill"
-            style={{ fontSize: 25 }}
-            title="twitter-logo"
-          ></i>
-        </a>
-      </Footer>
+      <Wrapper>
+        <TextWrapper>
+          Created by <a href="https://twitter.com/chemtocode">ChemToCode</a>
+        </TextWrapper>
+        <FooterItems>
+          <a
+            href="https://twitter.com/console.clear"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <RiTwitterFill size="25" title="twitter logo" />
+          </a>
+          <a
+            href="https://instagram.com/consoleclear"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <RiInstagramFill size="25" title="instagram logo" />
+          </a>
+        </FooterItems>
+      </Wrapper>
     </>
   );
 }
 
-const Footer = styled.div``;
+const Wrapper = styled.div`
+  display: grid;
+  width: 100%;
+  gap: 30px;
+  grid-template-columns: 25% auto;
+  align-items: center;
+`;
+
+const FooterItems = styled.div`
+  display: grid;
+  justify-content: end;
+  grid-template-columns: auto auto;
+  padding: 10px;
+  gap: 20px;
+
+  a {
+    text-decoration: none;
+    color: white;
+  }
+
+  a:nth-child(1) :hover {
+    color: #00acee;
+  }
+
+  a:nth-child(2) :hover {
+    color: #ffdc80;
+  }
+`;
+
+const TextWrapper = styled.p`
+  color: white;
+  a {
+    color: white;
+    text-decoration: none;
+  }
+`;
