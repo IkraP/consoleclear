8a8ec52d6efd61978d118aa06fa2742990e96be1
ikraP <ikrapervaiz@gmail.com>
2021-01-10 14:58:32 +0000

    add page transitions for both pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,28 @@ import * as React from "react";
 import { Layout } from "../components/layouts/layout";
 import Nightscene from "../components/sections/NightScene";
 import Seo from "../components/sections/Seo";
+import PageTransition from "gatsby-plugin-page-transitions";
 
 export default function IndexPage({ location }) {
   return (
     <>
-      <Seo title="ConsoleClear | Home" pathname={location.pathname} />
-      <Layout>
-        <Nightscene />
-      </Layout>
+      <PageTransition
+        defaultStyle={{
+          transition: `all 1s ease-in-out`,
+        }}
+        transitionStyles={{
+          entering: { opacity: 0.9 },
+          entered: { opacity: 1 },
+          exiting: { opacity: 1 },
+          exited: { opacity: 0.9 },
+        }}
+        transitionTime={200}
+      >
+        <Seo title="ConsoleClear | Home" pathname={location.pathname} />
+        <Layout>
+          <Nightscene />
+        </Layout>
+      </PageTransition>
     </>
   );
 }
--- src/pages/takeabreak.js
+++ src/pages/takeabreak.js
@@ -3,15 +3,29 @@ import { Layout } from "../components/layouts/layout";
 import Break from "../components/sections/Break";
 import BreakScene from "../components/sections/BreakScene";
 import Seo from "../components/sections/Seo";
+import PageTransition from "gatsby-plugin-page-transitions";
 
 export default function TakeABreak({ location }) {
   return (
     <>
-      <Seo title="ConsoleClear | Break" pathname={location.pathname} />
-      <Layout>
-        <Break />
-        <BreakScene />
-      </Layout>
+      <PageTransition
+        defaultStyle={{
+          transition: `all 1s ease-in-out`,
+        }}
+        transitionStyles={{
+          entering: { opacity: 0.9 },
+          entered: { opacity: 1 },
+          exiting: { opacity: 1 },
+          exited: { opacity: 0.9 },
+        }}
+        transitionTime={200}
+      >
+        <Seo title="ConsoleClear | Break" pathname={location.pathname} />
+        <Layout>
+          <Break />
+          <BreakScene />
+        </Layout>
+      </PageTransition>
     </>
   );
 }
