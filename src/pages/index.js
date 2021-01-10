f696f718498d43330017ff5e69370b737a99e79e
ikraP <ikrapervaiz@gmail.com>
2021-01-10 00:32:05 +0000

    add Seo component to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import * as React from "react";
 import { Layout } from "../components/layouts/layout";
 import Nightscene from "../components/sections/NightScene";
+import Seo from "../components/sections/Seo";
 
-export default function IndexPage() {
+export default function IndexPage({ location }) {
   return (
-    <Layout>
-      <Nightscene />
-    </Layout>
+    <>
+      <Seo title="ConsoleClear | Home" pathname={location.pathname} />
+      <Layout>
+        <Nightscene />
+      </Layout>
+    </>
   );
 }
