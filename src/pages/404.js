f73636cdc3d233dd0ec22b5b20aa6ce28ce1d78b
ikraP <ikrapervaiz@gmail.com>
2021-01-10 18:19:06 +0000

    add breakpoints and mobile and desktop scene

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,26 +1,43 @@
 import * as React from "react";
 import { Link } from "gatsby";
 import styled from "styled-components";
-import Nightscene from "../components/sections/NightSceneDesktop";
+import NightSceneDesktop from "../components/sections/NightSceneDesktop";
+import NightSceneMobile from "../components/sections/NightSceneMobile";
 import { Layout } from "../components/layouts/layout";
 import "@fontsource/arima-madurai";
 import Seo from "../components/sections/Seo";
+import PageTransition from "gatsby-plugin-page-transitions";
+import { useBreakpoint } from "gatsby-plugin-breakpoints";
 
 export default function NotFoundPage({ location }) {
+  const breakpoints = useBreakpoint();
   return (
     <>
-      <Seo title="ConsoleClear | 404" pathname={location.pathname} />
-      <Wrapper>
-        <Layout>
-          <TextWrapper>
-            <Text>404 - Page not found</Text>
-            <Link to="/">
-              <LinkText>Go back to home</LinkText>
-            </Link>
-          </TextWrapper>
-          <Nightscene />
-        </Layout>
-      </Wrapper>
+      <PageTransition
+        defaultStyle={{
+          transition: `all 1s ease-in-out`,
+        }}
+        transitionStyles={{
+          entering: { opacity: 0.9 },
+          entered: { opacity: 1 },
+          exiting: { opacity: 1 },
+          exited: { opacity: 0.9 },
+        }}
+        transitionTime={200}
+      >
+        <Seo title="ConsoleClear | 404" pathname={location.pathname} />
+        <Wrapper>
+          <Layout>
+            <TextWrapper>
+              <Text>404 - Page not found</Text>
+              <Link to="/">
+                <LinkText>Go back to home</LinkText>
+              </Link>
+            </TextWrapper>
+            {breakpoints.md ? <NightSceneMobile /> : <NightSceneDesktop />}
+          </Layout>
+        </Wrapper>
+      </PageTransition>
     </>
   );
 }
